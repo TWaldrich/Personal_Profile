cdb34f8f1ba3e45e67a22cd006b3c3eb98c21124
ThiagoWaldrich <thiagowlf7@gmail.com>
2023-07-31 20:28:50 -0300

    updated

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,19 +1,12 @@
-const AboutContainer = () =>{
-    return(
+const AboutContainer = () => {
+    return (
         <section className="about-container">
             <h2>Sobre</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Repellendus dicta maiores obcaecati, corporis nesciunt
-                vero magnam deserunt, harum illo veritatis quos error facere, 
-                consequatur doloremque architecto odit suscipit 
-                perspiciatis doloribus.
-            </p>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing 
-                elit. Distinctio commodi eum vitae eligendi explicabo
-                 labore vero minima, accusamus incidunt repudiandae, 
-                 natus asperiores assumenda ipsam quae. Hic a natus 
-                 provident fuga!
+            <p>Minha jornada na programação começou em 2019, durante o curso técnico integrado em eletrônica. Naquela época, tive a oportunidade de trabalhar com Arduino, o que despertou meu interesse no desenvolvimento de sistemas. Essa experiência me levou a ingressar no curso superior de Análise e Desenvolvimento de Sistemas, onde pude aprofundar meus conhecimentos em programação.
+                Decidi me concentrar no desenvolvimento front-end e mobile para expandir minhas habilidades. Tenho me dedicado ao estudo de tecnologias como Next.js, React.js e React Native, buscando dominar essas ferramentas e colocá-las em prática em projetos pessoais e acadêmicos.
+                Nessa nova jornada, estou animado para aprofundar meus conhecimentos e construir interfaces interativas e responsivas. Tenho aprendido sobre a criação de aplicações web com o Next.js e React.js, bem como o desenvolvimento de aplicativos móveis usando o React Native.
+                Além disso, também estou atento às tendências e melhores práticas do mercado, sempre buscando aprimorar minha experiência em UX/UI e adotar abordagens inovadoras no desenvolvimento de interfaces.
+                Com essa dedicação ao desenvolvimento front-end, espero ampliar meu conjunto de habilidades e me tornar um profissional completo e versátil na área de desenvolvimento de sistemas, com capacidade para trabalhar tanto na camada de front-end quanto de back-end.
             </p>
         </section>
     )
--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -3,12 +3,14 @@ const TechnologiesContainer = () =>{
         <section className="technologies-container">
             <h2>Tecnologias</h2>
             <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing 
-                elit. Deserunt perspiciatis ut, architecto voluptatum
-                nulla corrupti aspernatur maxime, at repellendus 
-                fugiat dignissimos eveniet incidunt? Dicta molestiae
-                ullam esse vero sapiente perferendis?
+                Como mencionado anteriormente, desde meu primeiro contato
+                com <strong>arduino</strong> eu passei a desenvolver com uma gama de tecnologias.
+                Já desenolvi aplicações completas utilizando <strong>React</strong> e <strong>Spring boot</strong>, realizei
+                o desenvolvimento de teplates web e mobile com o <strong>Figma</strong>. Estudei <strong>Python </strong>
+                durante a faculdade e atualmente estou me aprofundando em <strong>NextJS</strong> e <strong>React Native</strong>
+                
             </p>
+            
         </section>
     )
 }
